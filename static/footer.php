ca198a1fba4ac60b7f1532ad4e6df8bfe7b4fcdb
BALLOUARD JULIEN p2006861 <julien.ballouard@etu.univ-lyon1.fr>
2022-05-20 20:29:51 +0200

    finito

--- static/footer.php
+++ static/footer.php
@@ -1,5 +1,5 @@
 <footer>
 	<span> Auteurs: Julien et Matis </span> - <span> Année de réalisation: 2022 </span><br>
-	<span><a href="#">Plan du site</a></span><br>
+	<span><a href="plansite.txt">Plan du site</a></span><br>
 	<span><a href="https://www.univ-lyon1.fr/">Site de l'UCBL</a></span>
 </footer>
